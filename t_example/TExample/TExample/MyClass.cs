6ab9a9a011a8aca2a421114e4b7bd2cc62746038
ttomii <tatsuru_tomii@kk-grp.jp>
2014-07-15 08:55:37 +0900

    add example

---
+++ t_example/TExample/TExample/MyClass.cs
@@ -0,0 +1,14 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace TExample
+{
+    /// <summary>
+    /// Description of MyClass.
+    /// </summary>
+    public class MyClass
+    {
+        
+    }
+}
