e8c0e6272bc082a34589a434a2955322954ea6ee
fujihirakei <kei_fujihira@kk-grp.jp>
2014-07-25 18:24:00 +0900

    add an answer

---
+++ t2/answer/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace answer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void textbox_MouseDown(object sender, RoutedEventArgs e)
+        {
+            System.Windows.MessageBox.Show("Hello World!");
+            e.Handled = true;
+        }
+        void textbox_MouseUp(object sender, RoutedEventArgs e)
+        {
+            System.Windows.MessageBox.Show("HELLO WORLD!");
+            e.Handled = true;
+        }
+        void textbox_MouseDoubleClick(object sender, RoutedEventArgs t)
+        {
+            MessageBox.Show("hello");
+            t.Handled = true;
+
+        }
+    }
+}
