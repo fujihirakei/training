cb8614e0075b4cf516d58c1bdd7dad814205d3b0
fujihirakei <kei_fujihira@kk-grp.jp>
2014-07-25 18:29:04 +0900

    add an answer

---
+++ t3/answer/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace answer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        void textbox_MouseUp(object sender, RoutedEventArgs e)
+        {
+            int a = int.Parse(textbox1.Text);
+            int b = int.Parse(textbox2.Text);
+            int c = a + b;
+            string d = c.ToString();
+            MessageBox.Show(d);
+            e.Handled = true;
+        }
+    }
+}
